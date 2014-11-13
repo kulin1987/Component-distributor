d64ce21ceceda126f59c56c994f55c9afd17c104
Ventsislav Kulin <kulin1987@gmail.com>
2014-11-13 12:22:38 +0200

    AutoMapper integrated
    Created library ComponentDitributor.Web.Infrastructure with folders -
    Mapping, HtmlHelpers and Filters

---
+++ ComponentDistributorProject/ComponentDistributor.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace ComponentDistributor.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IConfiguration configuration);
+    }
+}
---
+++ ComponentDistributorProject/ComponentDistributor.Web.Infrastructure/Mapping/IMapFrom.cs
@@ -0,0 +1,6 @@
+﻿namespace ComponentDistributor.Web.Infrastructure.Mapping
+{
+    public interface IMapFrom<T>
+    {
+    }
+}
