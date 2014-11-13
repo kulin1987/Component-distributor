ca9f5eaca32d40abb88d3cb92b4d832a28911d9e
Ventsislav Kulin <kulin1987@gmail.com>
2014-11-13 11:53:13 +0200

    Integrated Ninject
    Added BaseController

---
+++ ComponentDistributorProject/ComponentDistributor.Web/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿namespace ComponentDistributor.Web.Controllers
+{    
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using ComponentDistributor.Data.Contracts.Repository;
+
+    public class BaseController : Controller
+    {
+        public BaseController(IDataProvider data)
+        {
+            this.Data = data;
+        }
+
+        public IDataProvider Data { get; set; }
+    }
+}
