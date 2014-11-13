758e160bc4c4f788011eefcd4dceb52746acb2f1
Ventsislav Kulin <kulin1987@gmail.com>
2014-11-13 13:04:46 +0200

    Small fix

--- ComponentDistributorProject/ComponentDistributor.Web/Global.asax.cs
+++ ComponentDistributorProject/ComponentDistributor.Web/Global.asax.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace ComponentDistributor.Web
+﻿namespace ComponentDistributor.Web
 {
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using ComponentDistributor.Web.Infrastructure.Mapping;
+    using System.Reflection;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -16,6 +16,9 @@ namespace ComponentDistributor.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            autoMapperConfig.Execute();
         }
     }
 }
